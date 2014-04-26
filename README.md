97cdc4c1ca9505c981c65004f3877d2e28270e4a
saravana0129 <saravana.29g@gmail.com>
2014-04-25 23:07:42 -0700

    Sub branch

---
+++ README.md
@@ -0,0 +1,5 @@
+MLKApp
+======
+Open maps.html to view current overlays
+
+![alt tag](screenshots/2014-04-06.png)
---
+++ bookNumberIndex.js
@@ -0,0 +1,186 @@
+var bookNumberIndex = {
+    "Thesis A - BL": {
+        "floor": 6,
+        "lat": 37.335374,
+        "long": -121.884666,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis BM": {
+        "floor": 6,
+        "lat": 37.335196,
+        "long": -121.884716,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis CJ - DT": {
+        "floor": 6,
+        "lat": 37.335295,
+        "long": -121.884921,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis E - HB": {
+        "floor": 6,
+        "lat": 37.335409,
+        "long": -121.885174,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis HC - HM": {
+        "floor": 6,
+        "lat": 37.335620,
+        "long": -121.885166,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis HQ - JN": {
+        "floor": 7,
+        "lat": 37.335410,
+        "long": -121.884713,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis JQ": {
+        "floor": 7,
+        "lat": 37.335158,
+        "long": -121.884745,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis LB": {
+        "floor": 7,
+        "lat": 37.335317,
+        "long": -121.884949,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis PA - PR": {
+        "floor": 7,
+        "lat": 37.335507,
+        "long": -121.885339,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis PR - PZ": {
+        "floor": 7,
+        "lat": 37.335627,
+        "long": -121.885193,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis QD": {
+        "floor": 8,
+        "lat": 37.335132,
+        "long": -121.884773,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis R - S": {
+        "floor": 8,
+        "lat": 37.335321,
+        "long": -121.884951,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Thesis TK - Z": {
+        "floor": 8,
+        "lat": 37.335477,
+        "long": -121.885258,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Folio N": {
+        "floor": 8,
+        "lat": 37.335570,
+        "long": -121.884987,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Folio G": {
+        "floor": 8,
+        "lat": 37.335645,
+        "long": -121.885343,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Folio F": {
+        "floor": 8,
+        "lat": 37.335845,
+        "long": -121.885279,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Folio A": {
+        "floor": 8,
+        "lat": 37.335481,
+        "long": -121.885491,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Fiction": {
+        "floor": 3,
+        "lat": 37.335382,
+        "long": -121.884670,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Non-Fiction": {
+        "floor": 3,
+        "lat": 37.335429,
+        "long": -121.885172,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "J Fiction": {
+        "floor": 3,
+        "lat": 37.335731,
+        "long": -121.884997,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "Ethnic Studies": {
+        "floor": 5,
+        "lat": 37.335355,
+        "long": -121.884992,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "A - JA": {
+        "floor": 2,
+        "lat": 37.335718,
+        "long": -121.885022,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    },
+    "JA - Z": {
+        "floor": 2,
+        "lat": 37.335556,
+        "long": -121.885383,
+        "contentString": '<div id="content" style="color:black;font-family:verdana;">' +
+            "Book Location" +
+            '</div>'
+    }
+}
